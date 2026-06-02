8425c84e038f7783fe85d2904f0e1a289c0da5b3
Fernando Silva <tchfernando@gmail.com>
2026-06-02 12:31:28 -0300

    test(e2e): cover complete quiz workflow

--- cypress/e2e/quiz-flow.cy.ts
+++ cypress/e2e/quiz-flow.cy.ts
@@ -4,34 +4,36 @@ describe('Quiz Flow', () => {
 
     cy.visit('/', {
       onBeforeLoad(win) {
-
         win.localStorage.setItem(
           'studyBuddy.progress.v1',
           JSON.stringify({
             'lesson-alg-1': 100
           })
         );
-
       }
     });
 
   });
 
-  it('completes a quiz', () => {
+  it('completes an entire quiz', () => {
 
     cy.visit('/quiz');
 
-    cy.contains('Start')
+    cy.get('[data-cy="start-quiz"]')
       .click();
 
-    cy.get('mat-radio-button')
-      .first()
-      .click();
+    for (let i = 0; i < 3; i++) {
 
-    cy.contains('Submit')
-      .click();
+      cy.get('mat-radio-button')
+        .first()
+        .click();
 
-    cy.contains('Question')
+      cy.get('[data-cy="submit-answer"]')
+        .click();
+
+    }
+
+    cy.get('[data-cy="quiz-completed"]')
       .should('exist');
 
   });
