4645f2bfd14ef6dc096c3af3f3c899117313d254
Fernando Silva <tchfernando@gmail.com>
2026-06-02 11:16:19 -0300

    chore(config): add correct application host

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:4200',
   },
 });
