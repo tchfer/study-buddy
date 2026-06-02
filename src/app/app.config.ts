b1f117daadd866cc3c6d71a15bda5e32a61c8369
Fernando Silva <tchfernando@gmail.com>
2026-06-02 09:53:21 -0300

    feat(http): add request logging interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
 
 import { routes } from './app.routes';
+import { loggingInterceptor } from './core/interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        loggingInterceptor
+      ])
+    ),
     {
       provide: MAT_ICON_DEFAULT_OPTIONS,
       useValue: { fontSet: 'material-symbols-outlined' },
---
+++ src/app/core/interceptors/logging.interceptor.ts
@@ -0,0 +1,9 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const loggingInterceptor: HttpInterceptorFn = (req, next) => {
+  console.log(
+    `[HTTP] ${req.method} ${req.url}`
+  );
+
+  return next(req);
+};
